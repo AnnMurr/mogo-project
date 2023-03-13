a6857fdfeb1d469afdc25c39b5950bdab758afe5
AnnMurr <annmurina111@gmail.com>
2023-03-13 21:11:41 +0200

    added adaptation 320

--- core/slider-swiper.js
+++ core/slider-swiper.js
@@ -3,6 +3,31 @@ new Swiper ('.reviews__swiper-container', {
     nextEl: '.reviews__btn-right',
     prevEl: '.reviews__btn-left'
     },
+
+    pagination: {
+        el: '.swiper-pagination',
+
+        clickable: true,
+    },
+    
+    loop: true,
+
+    spaceBetween: 20,
+    
+    speed: 800
+});
+
+new Swiper ('.reviews-second__swiper-container', {
+    navigation: {
+    nextEl: '.reviews-second__btn-right',
+    prevEl: '.reviews-second__btn-left'
+    },
+
+    pagination: {
+        el: '.swiper-second-pagination',
+
+        clickable: true,
+    },
     
     loop: true,
 
